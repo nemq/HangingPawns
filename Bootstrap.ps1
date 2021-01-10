322c3bc63a0bfa399a51121aa9aa98557f9bd4c7
Tomasz Sawka <sawka.tomek@gmail.com>
2021-01-10 20:31:48 +0100

    Add boost-algorithms to dependency package.
    Update Bootstrap.ps1 to download updated vcpkg export including boost.
    Rename packages directory from gtest to more general vcpkg.
    Updae Readme.md with vcpkg export preparation.

--- Bootstrap.ps1
+++ Bootstrap.ps1
@@ -1,7 +1,16 @@
-New-Item -ItemType Directory -Path .\packages
+if (Test-Path .\packages) {
+    Remove-Item -Recurse -Force .\packages\vcpkg
+}
+else {
+    New-Item -ItemType Directory -Path .\packages
+}
+
 #IMPORTANT: change dl=0 to dl=1 
-$URL = "https://www.dropbox.com/s/g847uh50hgzafve/v0.0.0.zip?dl=1";
-Invoke-WebRequest -Uri $URL  -OutFile .\packages\gtest.zip      
-Expand-Archive -Path .\packages\gtest.zip  -DestinationPath .\packages
-Remove-Item -Path .\packages\gtest.zip
+$URL = "https://www.dropbox.com/s/lomvz29ndcqaahw/vcpkg-export-20210110-193925.zip?dl=1";
+Invoke-WebRequest -Uri $URL  -OutFile .\packages\vcpkg.zip      
+Expand-Archive -Path .\packages\vcpkg.zip  -DestinationPath .\packages
+Remove-Item -Path .\packages\vcpkg.zip
+$dirName = Get-ChildItem -Path .\packages\ -Directory | Select-Object -ExpandProperty Name 
+Rename-Item -Path ".\packages\$dirName" -NewName "vcpkg"
+
 
--- Readme.md
+++ Readme.md
@@ -5,6 +5,15 @@ Simple tool to find C++ files with least number of dependencies - "HangingPawns"
 HangingPawns.exe C:\path\to\project\directory
 
 ### Dev usage:
+
 1. Run Bootstrap.ps1 to download dependencies
 2. Install "Test Adapter for Google Test"
 3. In Visual Studio set Tool -> Options -> Test Adapter for Google Test -> Regex for test discovery = ".*Tests.*\.exe"
+
+### Packaging dependencies
+
+0. vcpkg.exe install  boost-algorithm:x64-windows gtest:x64-windows
+1. vcpkg.exe export --raw boost-algorithm:x64-windows gtest:x64-windows boost-algorithm:x64-windows gtest:x64-windows
+2. Zip
+3. Upload to dropbox
+4. Update Bootstrap.ps1 $URL
